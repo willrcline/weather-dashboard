6261f9a5f24849799baf12e32dc800adc3aa7edc
willcline613 <willrcline.atx@gmail.com>
2023-04-30 20:42:41 -0500

    Fixed date issue by calling one api for today and another for the upcoming 5 days. upcoming one uses every 8th list element that comes back from the data because there is a 3 hour step and thus 8 list items per 24 hours. Added icon image to list of data points rendered for each day. Added fancy fonts.

---
+++ README.md
@@ -0,0 +1,10 @@
+### Api Calls:
+This application leverages [openWeathermap API](https://openweathermap.org/forecast5)
+2 Api calls are made using js: one to get coordinates for that zip code and the other to get the weather data for those coords.
+
+### Location search:
+It displays the weather data for the chosen zip code for today and the following 5 days.
+It saves the previous searches to localStorage and those are clickable to get the search for that location displayed again.
+
+### Future: 
+Add faded gif for background based on weather conditions (ie. rainy= rainy day gif)
--- assets/script.js
+++ assets/script.js
@@ -15,42 +15,51 @@ async function getCoordsForZipCode(zipCode) {
     return data.coord
 }
 
-async function callWeatherApi(lat, lon) {
+async function callForecast5Api(lat, lon) {
     var forecast5Url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`
     const response = await fetch(forecast5Url);
     const weatherData = await response.json();
     return weatherData
 }
 
-async function getWeatherData(zipCode) {
-    var coordinates = await getCoordsForZipCode(zipCode)
-    var weatherData = await callWeatherApi(coordinates.lat, coordinates.lon)
+async function callTodayWeatherApi(lat, lon) {
+    var todayWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`
+    const response = await fetch(todayWeatherUrl);
+    const weatherData = await response.json();
     return weatherData
 }
 
-function renderToday(weatherData) {
-    var today = weatherData.list[0]
+
+function renderToday(data) {
+    console.log("renderToday_________")
     var todaySection = $("#today")
     todaySection.empty()
 
     var div = $('<div>')
-    var date = $('<h3>').text(`Today in ${weatherData.city.name}, ${today.dt_txt.split(" ")[0]}`)
-    var ul = createUlWithDayData(today)
-    div.append(date, ul)
+    var header = $('<h3>').text(`Today in ${data.name}`)
+    
+    // var ul = $("<ul>")
+    // var description = $("<li>").text(data.weather[0].description)
+    // // var iconImg = $("<img>")
+    // var temp = $("<li>").text(day.main.temp)
+    // var wind = $("<li>").text(day.wind.speed)
+    // var humidity = $("<li>").text(day.main.humidity)
+    var ul = createUlWithDayData(data)
+    div.append(header, ul)
     todaySection.append(div)
 }
 
 function renderFiveDay(weatherData) {
-    
-    var fiveDayData = weatherData.list.slice(1,6)
+    var fiveDayData = weatherData.list
     var fiveDaySection = $("#five-day")
     fiveDaySection.empty()
-    
-    for (let day of fiveDayData) {
+    for (i=0; i<weatherData.list.length; i+=8) {
+        var day = weatherData.list[i]
         var date = day.dt_txt.split(" ")[0]
+        var formattedDate = dayjs(date).format("MMM D")
         var dayOfWeek = dayjs(date).format('dddd')
         var div = $('<div>')
-        var date = $('<h3>').text(`${dayOfWeek} ${date}`)
+        var date = $('<h3>').text(`${dayOfWeek}, ${formattedDate}`)
         var ul = createUlWithDayData(day)
         div.append(date, ul)
         fiveDaySection.append(div)
@@ -59,13 +68,18 @@ function renderFiveDay(weatherData) {
 
 function createUlWithDayData(day) {
     var ul = $("<ul>")
-    var description = $("<li>").text(day.weather[0].description)
-    // var iconImg = $("<img>")
-    var temp = $("<li>").text(day.main.temp)
-    var wind = $("<li>").text(day.wind.speed)
-    var humidity = $("<li>").text(day.main.humidity)
-
-    ul.append(description, temp, wind, humidity)
+    var iconImg = $("<img>").attr("src", `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`).css({
+        'width': '45px',
+        'height': '45px'  
+    });
+    var icon = $("<li>")
+    var description = $("<li>").css({"font-family": "'Taviraj', serif", 'font-weight': 'bold'}).text((day.weather[0].description).toUpperCase())
+    var temp = $("<li>").text(`Temperature: ${day.main.temp}`)
+    var wind = $("<li>").text(`Wind Speed: ${day.wind.speed}`)
+    var humidity = $("<li>").text(`Humidity: ${day.main.humidity}`)
+    
+    icon.append(iconImg)
+    ul.append(icon, description, temp, wind, humidity)
     return ul
 }
 
@@ -75,13 +89,11 @@ function addLocationToLocalStorage(zipCode, city) {
 
     var locationListWithPotentialDuplicates = getLocationListFromLocalStorage()
     
-    
     locationListWithPotentialDuplicates.unshift(location)
     //?Written with assistance of AI tool.
     //inner function (findIndex) loops through the locationList objects and returns true for ones where the value is the same as the one selected in the outer loop. The index is retreived of first one that returns true. This index is compared to the index of the currently selected object in the outer loop. If they are not the same index (and we already know they are the same value because the inner loop checked for that), then it is a duplicate, and thus ought to return false to the filter method so that it is removed in the resulting array.
     let locationList = locationListWithPotentialDuplicates.filter((value, index, self) => 
     index === self.findIndex((t) => (t.city === value.city)))
-    console.log(locationList)
     
     localStorage.setItem("locationList", JSON.stringify(locationList))
 }
@@ -108,13 +120,16 @@ function renderSearchHistory() {
 }
 
 async function searchAndRender(zipCode) {
-    var weatherData = await getWeatherData(zipCode)
-    var city = weatherData.city.name
-    console.log(zipCode, city)
+    var coordinates = await getCoordsForZipCode(zipCode)
+    var forecast5Data = await callForecast5Api(coordinates.lat, coordinates.lon)
+    var todayWeatherData = await callTodayWeatherApi(coordinates.lat, coordinates.lon)
+    console.log(todayWeatherData)
+    var city = forecast5Data.city.name
+    // // console.log(zipCode, city)
     addLocationToLocalStorage(zipCode, city)
     renderSearchHistory()
-    renderToday(weatherData)
-    renderFiveDay(weatherData)
+    renderToday(todayWeatherData)
+    renderFiveDay(forecast5Data)
 }
 
 $("button").on("click", () => {
